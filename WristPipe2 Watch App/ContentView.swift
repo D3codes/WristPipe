f2d246d44613ee3a62b3baddbf8db542f5a7e556
David Freeman <david@d3.codes>
2024-02-25 07:40:23 -0600

    Initial Commit

---
+++ WristPipe2 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WristPipe2 Watch App
+//
+//  Created by David Freeman on 2/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WristPipe2 Watch App/WristPipe2App.swift
@@ -0,0 +1,17 @@
+//
+//  WristPipe2App.swift
+//  WristPipe2 Watch App
+//
+//  Created by David Freeman on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WristPipe2_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
